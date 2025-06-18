fc60fbf2fb58272d54e40fed53ce0bf4cb2d670a
Garrett Fincke <garrettfincke@gmail.com>
2025-06-17 20:35:20 -0400

    Deleted 20250617000001_add_basket_insert_policy.sql

--- .bolt/supabase_discarded_migrations/20250617000001_add_basket_insert_policy.sql
+++
@@ -1,5 +0,0 @@
--- Migration: supabase/migrations/20250617000001_add_basket_insert_policy.sql
--- Add missing INSERT policy for baskets table; this allows authenticated users to create new baskets
-
-CREATE POLICY "Users can create baskets" ON baskets
-    FOR INSERT WITH CHECK ((select auth.uid()) IS NOT NULL); 
