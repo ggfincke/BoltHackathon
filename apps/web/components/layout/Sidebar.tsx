192f8229b848ef3edbeafbc1f61ff337e54ceed9
Garrett Fincke <garrettfincke@gmail.com>
2025-06-28 21:44:48 -0400

    reorganized Navbar to put more emphasis on searchbar, removed collapsing from Sidebar

--- apps/web/components/layout/Sidebar.tsx
+++ apps/web/components/layout/Sidebar.tsx
@@ -180,7 +180,7 @@ export default function Sidebar({ variant = 'home' }: SidebarProps) {
       }`}
     >
       <div className="flex flex-col h-full">
-        {/* Collapse Toggle */}
+        {/* Collapse Toggle
         <div className="p-2 sidebar-section">
           <button
             onClick={() => setIsCollapsed(!isCollapsed)}
@@ -197,7 +197,7 @@ export default function Sidebar({ variant = 'home' }: SidebarProps) {
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 19l-7-7 7-7m8 14l-7-7 7-7" />
             </svg>
           </button>
-        </div>
+        </div> */}
 
         <div className="flex-1 overflow-y-auto">
           {/* Home Section */}
